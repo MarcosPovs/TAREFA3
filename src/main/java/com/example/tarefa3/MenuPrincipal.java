2ea844db6d763b9da97a4fb608ed5b0a383f96ca
MarcosPovs <163356276+MarcosPovs@users.noreply.github.com>
2024-10-11 18:58:44 -0300

    Update Layout Menu
    Update do layout da aba carros

--- src/main/java/com/example/tarefa3/MenuPrincipal.java
+++ src/main/java/com/example/tarefa3/MenuPrincipal.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Principal extends Application {
+public class MenuPrincipal extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
